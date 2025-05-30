b6a06f7587d478ec6ed3ab1a0b59ff6ad45b718d
akumar <akumar@trapptechnology.com>
2025-05-29 22:23:57 -0700

    fix

--- nDPId.c
+++ nDPId.c
@@ -4028,9 +4028,9 @@ static void ndpi_process_packet(uint8_t * const args,
     if (measuring == 0)
     {
         start_time = now;
-        total_bytes = {0};
-        packet_count = {0};
-        measuring = 1;
+        //total_bytes = {0};
+        //packet_count = {0};
+        //measuring = 1;
         printf("Started measuring...\n");
     }
 
