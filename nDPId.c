87286f741170d19b02dd4ba23538da6583fb1af9
akumar <akumar@trapptechnology.com>
2025-06-16 01:56:08 -0700

    more logging

--- nDPId.c
+++ nDPId.c
@@ -4158,7 +4158,7 @@ void ndpi_process_packet_consumer(uint8_t * const args,
     if (start_time == 0)
     {
         // First call: start the timer
-        printf("capturing packets for 60 seconds...\n");
+        printf("Ashwani: capturing packets for 60 seconds...\n");
         start_time = now;
     }
     else if (difftime(now, start_time) >= 60)
@@ -4169,13 +4169,13 @@ void ndpi_process_packet_consumer(uint8_t * const args,
         start_time = 0;
     }
 
-    printf("\Ashwani 11");
+    printf("\nAshwani 11");
 
     workflow->packets_captured++;
 
-    printf("\Ashwani 12");
+    printf("\nAshwani 12");
     time_us = ndpi_timeval_to_microseconds(header->ts);
-    printf("\Ashwani 13");
+    printf("\nAshwani 13");
     if (workflow->last_global_time < time_us)
     {
         workflow->last_global_time = time_us;
@@ -4185,13 +4185,13 @@ void ndpi_process_packet_consumer(uint8_t * const args,
         workflow->last_thread_time = time_us;
     }
 
-    printf("\Ashwani 1");
+    printf("\nAshwani 1");
     do_periodically_work(reader_thread);
 
-    printf("\Ashwani 2");
+    printf("\nAshwani 2");
     if (process_datalink_layer(reader_thread, header, packet, &ip_offset, &type, &flow_basic.vlan_id) != 0)
     {
-        printf("\Ashwani 3");
+        printf("\nAshwani 3");
         return;
     }
 
