1db1299d19339130d37095f5c02946df96e41ac0
akumar <akumar@trapptechnology.com>
2025-11-10 23:16:51 -0800

    dsd

--- nDPId_pcap.c
+++ nDPId_pcap.c
@@ -139,6 +139,7 @@ static inline uint64_t mt_pt_get_and_sub(volatile uint64_t * value, uint64_t sub
 #include "../json-c/include/json-c/json.h"
 
 #include <dirent.h>
+#include "utils.c"
 
 #define PATH_MAX_LEN 1024
 #define MAX_FILENAME_LEN 512
