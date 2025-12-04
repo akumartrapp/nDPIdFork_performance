a4e1ce00f34f947d1b8bd6178595fe4419581f12
akumar <akumar@trapptechnology.com>
2025-12-03 19:58:54 -0800

    write_to_file in nDPId_pcap

--- nDPId_pcap.c
+++ nDPId_pcap.c
@@ -415,7 +415,7 @@ void write_to_file(const char * const json_msg, const char * const json_string_w
     char * converted_json_str = NULL;
     int flow_risk_count = 0;
 
-    ConvertnDPIDataFormat(json_msg, json_string_with_http_or_tls_info, &converted_json_str, &flow_risk_count);
+    ConvertnDPIDataFormat(json_msg, json_string_with_http_or_tls_info, 0, &converted_json_str, &flow_risk_count);
     if (converted_json_str != NULL)
     {
         int length = strlen(converted_json_str);
@@ -424,16 +424,39 @@ void write_to_file(const char * const json_msg, const char * const json_string_w
             if (flow_risk_count)
             {
                 write_to_alert_file(converted_json_str, length);
-                char * converted_json_str_no_risk = NULL;
-                DeletenDPIRisk(converted_json_str, &converted_json_str_no_risk);
-                int length_converted = strlen(converted_json_str_no_risk);
-                write_to_event_file(converted_json_str_no_risk, length_converted);
-                free(converted_json_str_no_risk);
+                for (size_t index = 1; index < flow_risk_count; index++)
+                {
+                    free(converted_json_str);
+                    size_t flow_risk_count_dummy = 0;
+                    ConvertnDPIDataFormat(json_msg,
+                                          json_string_with_http_or_tls_info,
+                                          index,
+                                          &converted_json_str,
+                                          &flow_risk_count_dummy);
+                    length = strlen(converted_json_str);
+                    if (length != 0)
+                    {
+                        write_to_alert_file(converted_json_str, length);
+                    }
+                }
+
+                if (length == 0)
+                {
+                    logger(1, "string length is 0");
+                }
+                else
+                {
+                    char * converted_json_str_no_risk = NULL;
+                    DeletenDPIRisk(converted_json_str, &converted_json_str_no_risk);
+                    int length_converted = strlen(converted_json_str_no_risk);
+                    write_to_event_file(converted_json_str_no_risk, length_converted);
+                    free(converted_json_str_no_risk);
+                }
             }
             else
             {
                 write_to_event_file(converted_json_str, length);
-            }            
+            }
         }
 
         free(converted_json_str);
