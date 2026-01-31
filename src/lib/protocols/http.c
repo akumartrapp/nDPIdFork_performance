4466f26eae246ffb6407255bcd6f9b287a6d70fc
akumar <akumar@trapptechnology.com>
2026-01-31 14:53:12 -0800

    libNDPI http.c updated 3

--- src/lib/protocols/http.c
+++ src/lib/protocols/http.c
@@ -219,7 +219,7 @@ static void ndpi_http_check_human_redeable_content(struct ndpi_detection_module_
 static void ndpi_validate_http_content(struct ndpi_detection_module_struct *ndpi_struct,
 				       struct ndpi_flow_struct *flow) {
 
-   printf("Ashwani: ndpi_validate_http_content called");
+  printf("Ashwani: ndpi_validate_http_content called");
   struct ndpi_packet_struct *packet = &ndpi_struct->packet;
   const u_int8_t *double_ret = (const u_int8_t *)ndpi_strnstr((const char *)packet->payload, "\r\n\r\n", packet->payload_packet_len);
 
