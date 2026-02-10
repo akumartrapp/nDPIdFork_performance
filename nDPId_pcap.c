22244cd3702f7e3254ac05998954744131d36907
akumar <akumar@trapptechnology.com>
2026-02-09 19:55:12 -0800

    zdzc

--- nDPId_pcap.c
+++ nDPId_pcap.c
@@ -4269,7 +4269,7 @@ static int jsonize_flow_event(struct nDPId_reader_thread * const reader_thread,
         return -1; 
     }
 
-    if (flow != NULL && flow->info != NULL && flow->info.detection_data != NULL) 
+    if (flow != NULL && flow->info.detection_data != NULL) 
     {
         ndpi_serialize_string_int32(&workflow->ndpi_serializer, "client_packet_direction", flow->info.detection_data->flow.client_packet_direction);
     }
