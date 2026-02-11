0f674a96966bc4a27dc013230a82e0669a2e77f5
akumar <akumar@trapptechnology.com>
2026-02-11 13:17:08 -0800

    error

--- nDPId_pcap.c
+++ nDPId_pcap.c
@@ -3747,12 +3747,13 @@ static void write_to_socket(struct nDPId_reader_thread * const reader_thread,
 }
 
 static void send_to_collector(struct nDPId_reader_thread * const reader_thread, char const * const json_msg, size_t json_msg_len, enum flow_event event)
+{
     // Use new APIs for flow direction tracking
     StoreOrUpdateFlowDirection(json_msg);
     char *updated_json = UpdateFlowDirectionIfSwapped(json_msg);
     const char * json_msg = updated_json ? updated_json : json_msg;
     size_t json_msg_len = updated_json ? strlen(updated_json) : json_msg_len;
-    {
+    
     write_to_console(0, "send_to_collector called");
     struct nDPId_workflow * const workflow = reader_thread->workflow;
     char newline_json_msg[NETWORK_BUFFER_MAX_SIZE];
