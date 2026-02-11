cf6d2ff871f248c5945b0ba050603069fe98509f
akumar <akumar@trapptechnology.com>
2026-02-10 22:48:13 -0800

    compiler

--- nDPId_pcap.c
+++ nDPId_pcap.c
@@ -3750,11 +3750,11 @@ static void send_to_collector(struct nDPId_reader_thread * const reader_thread, 
 {
     write_to_console(0, "send_to_collector called");
     struct nDPId_workflow * const workflow = reader_thread->workflow;
-    if (workflow->ndpi_struct.input_info.in_pkt_dir == NDPI_IN_PKT_DIR_C_TO_S)
+    if (workflow->ndpi_struct->input_info.in_pkt_dir == NDPI_IN_PKT_DIR_C_TO_S)
     {
         ndpi_serialize_string_string(&workflow->ndpi_serializer, "initiator", "NDPI_IN_PKT_DIR_C_TO_S");
     }
-    else if (workflow->ndpi_struct.input_info.in_pkt_dir == NDPI_IN_PKT_DIR_S_TO_C) 
+    else if (workflow->ndpi_struct->input_info.in_pkt_dir == NDPI_IN_PKT_DIR_S_TO_C) 
     {
         ndpi_serialize_string_string(&workflow->ndpi_serializer, "initiator", "NDPI_IN_PKT_DIR_S_TO_C");
     }
